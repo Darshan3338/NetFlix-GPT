48c4c47f825cceb281f004ac04b96a0af21c0e1f
Darshan3338 <darshancmp356@gmail.com>
2025-02-21 12:36:45 +0530

    TMDB API data Fetch &&  built MainContainer

---
+++ src/Components/MainContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import VideoBackground from './VideoBackground';
+import VideoTitle from './VideoTitle';
+
+
+const MainContainer = () => {
+    const movies= useSelector(store=>store.movies?.nowPlayingMovies)
+        if(!movies) return //or if(movies===null) return
+    const mainMovie = movies[0] //20movies contains in movies we need only one so [0]
+    // console.log("MainMovie",mainMovie)
+
+const {original_title,overview,id} = mainMovie
+
+    return (
+    <div>
+      <VideoBackground movie_id={id}/>
+      <VideoTitle title={original_title} overview={overview}/>
+    </div>
+  );
+}
+
+export default MainContainer;
